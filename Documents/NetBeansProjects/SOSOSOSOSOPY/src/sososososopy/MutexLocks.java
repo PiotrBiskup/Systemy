9476c65c061251427c38cc0202f543e6e7c93e12
pietrek663 <34544442+pietrek663@users.noreply.github.com>
2017-12-28 20:20:07 +0100

    Update MutexLocks.java

--- Documents/NetBeansProjects/SOSOSOSOSOPY/src/sososososopy/MutexLocks.java
+++ Documents/NetBeansProjects/SOSOSOSOSOPY/src/sososososopy/MutexLocks.java
@@ -14,7 +14,7 @@ public class MutexLocks
 	
 	public MutexLocks(String name) {
 		this.name = name;
-		this.setState(false);
+		this.setLockState(false);
 		waitingProcesses = new LinkedList<Process>();
 	}
 	
@@ -22,37 +22,37 @@ public class MutexLocks
 		return state;
 	}
 	
-	public void setState(boolean state)  {
+	public void setLockState(boolean state)  {
 		this.state = state;
 	}
 	
 	public void displayLockedProcess() {
-		System.out.println(lockedProcess.get_process_name());
+		System.out.println(lockedProcess.get_ID());
 	}
 	
 	public void lock(Process processToLock) {
 		if(!getState()) {
 			//zamek jest otwarty, proces zamyka zamek i rusza dalej
-			setState(true);
+			setLockState(true);
 			this.lockedProcess = processToLock;
 		} else {
 			//zamek jest zamkniety wiec proces wedruje do kolejki i ustawiany jest jego bit blocked
 			waitingProcesses.offer(processToLock);
-			processToLock.SetBlocked(true);  // to ustawiamy na true zeby wiadomo bylo ze proces jest zablokowany
-			processToLock.SetState(1); // to jest metoda od prcesow i nie wiem jeszcze jak oznaczyc proces czekajacy
+			processToLock.set_if_lock(1);  // to ustawiamy na true zeby wiadomo bylo ze proces jest zablokowany
+			processToLock.set_state(1); // to jest metoda od prcesow i nie wiem jeszcze jak oznaczyc proces czekajacy
 			
 		}
 	}
 	
 	public void unlock(Process processToUnlock) {
 		//odblokowuje zamek i jezli kolejka nie jest pusta to zeruje bit blocked pierwszego oczekujacego procesu.
-		if(processToUnlock.GetID() == this.lockedProcess.GetID()) {
-			this.setState(false);
+		if(processToUnlock.get_ID() == this.lockedProcess.get_ID()) {
+			this.setLockState(false);
 			this.lockedProcess = null;
 			if(!waitingProcesses.isEmpty()) 
                         {
-				waitingProcesses.peek().SetState(0); //to jest metoda od prcesow i tu zmieniamy procesowi na szczycie kolejki procesow czekajacych stan na ready
-				waitingProcesses.poll().SetBlocked(false); // to ustawiamy na false zeby wiadomo bylo ze proces juz nie jest zablokowany (funkcja poll() zwraca gore kolejki procesow czekajacych i usuwa go z niej)
+				waitingProcesses.peek().set_state(0); //to jest metoda od prcesow i tu zmieniamy procesowi na szczycie kolejki procesow czekajacych stan na ready
+				waitingProcesses.poll().set_if_lock(0); // to ustawiamy na false zeby wiadomo bylo ze proces juz nie jest zablokowany (funkcja poll() zwraca gore kolejki procesow czekajacych i usuwa go z niej)
 			}
 		}
 	} 
